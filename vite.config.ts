c79e1f6885300cc6eef4339713c4eb501021e3ea
s-weigand <s.weigand.phy@gmail.com>
2025-05-24 12:12:36 +0200

    ♻️ Rename project to clipboard-inspector

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,39 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: false,
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: "prompt",
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'what-is-in-my-clipboard',
-      short_name: '📋🕵',
-      description: 'Inspect what is on your clipboard',
-      theme_color: '#161616',
-    },
+      manifest: {
+        name: "clipboard-inspector",
+        short_name: "📋🕵",
+        description: "Inspect what is on your clipboard",
+        theme_color: "#161616",
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
-})
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
